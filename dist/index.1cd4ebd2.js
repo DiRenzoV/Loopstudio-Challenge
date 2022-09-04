715b206549154ba7482af82dc01466116747392a
Vinicius Di Renzo <direnzov@hotmail.com>
2022-09-04 08:28:14 +0100

    build for production.

---
+++ dist/index.1cd4ebd2.js
@@ -0,0 +1,2 @@
+const hamburger=document.querySelector("[data-hamburger]"),menu=document.querySelector("[data-menu]");hamburger.addEventListener("click",(()=>{menu.classList.toggle("open"),hamburger.classList.toggle("active")}));
+//# sourceMappingURL=index.1cd4ebd2.js.map
